b752720edc3df8875842ce9ed83b9914d758316d
ricardocollado <ricardocolladorothschild@gmail.com>
2023-09-09 01:38:13 -0400

    add the class SelectionClass.js there we put the two methods that handle the change of the div circle, and then create an object to access to the methods

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 let rate = 0;
 const circle_1 = document.querySelector('.circle1');
 const circle_2 = document.querySelector('.circle2');
@@ -10,69 +11,18 @@ const circle_5 = document.querySelector('.circle5');
 
 
 //functions
-function colorSelection(selection){
 
-    switch(selection){
-        case 1:
-            circle_1.classList.add('coloredSelection');
-            break;
-        case 2:    
-            circle_2.classList.add('coloredSelection');
-            break;
-        case 3:
-            circle_3.classList.add('coloredSelection');
-            break;
-        case 4: 
-            circle_4.classList.add('coloredSelection');
-            break;        
-    
-        case 5:
-            circle_5.classList.add('coloredSelection');
-            break;
-        }
-}
 
-function deSelect(currentSelection){
 
-        switch(currentSelection){
-
-            case 1:
-                circle_2.classList.remove('coloredSelection');
-                circle_3.classList.remove('coloredSelection');
-                circle_4.classList.remove('coloredSelection');
-                circle_5.classList.remove('coloredSelection');
-                break;
-            case 2:
-                circle_1.classList.remove('coloredSelection');
-                circle_3.classList.remove('coloredSelection');
-                circle_4.classList.remove('coloredSelection');
-                circle_5.classList.remove('coloredSelection');
-                break;
-            case 3:
-                circle_1.classList.remove('coloredSelection');
-                circle_2.classList.remove('coloredSelection');
-                circle_4.classList.remove('coloredSelection');
-                circle_5.classList.remove('coloredSelection');
-                break;
-            case 4:
-                circle_1.classList.remove('coloredSelection');
-                circle_2.classList.remove('coloredSelection');
-                circle_3.classList.remove('coloredSelection');
-                circle_5.classList.remove('coloredSelection');    
-                break;
-            case 5:
-                circle_1.classList.remove('coloredSelection');
-                circle_2.classList.remove('coloredSelection');
-                circle_3.classList.remove('coloredSelection');
-                circle_4.classList.remove('coloredSelection');  
-                break;  
-        }
-}
 
 function ratePicker(rating){
     rate = rating;
+    /*
     deSelect(rate);
-    colorSelection(rate);
+    colorSelection(rate);*/
+    let selection = new SelectionClass(rate);
+    selection.colorSelection(rate);
+    selection.deSelect(rate);
 }
 
 //events
