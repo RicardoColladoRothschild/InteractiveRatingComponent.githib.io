b8d11c9bb91d74935aba517268732a4151d701b3
ricardocollado <ricardocolladorothschild@gmail.com>
2023-09-09 01:06:03 -0400

    add events for each div from 1 to 5

--- main.js
+++ main.js
@@ -42,6 +42,30 @@ function deSelect(currentSelection){
                 circle_4.classList.remove('coloredSelection');
                 circle_5.classList.remove('coloredSelection');
                 break;
+            case 2:
+                circle_1.classList.remove('coloredSelection');
+                circle_3.classList.remove('coloredSelection');
+                circle_4.classList.remove('coloredSelection');
+                circle_5.classList.remove('coloredSelection');
+                break;
+            case 3:
+                circle_1.classList.remove('coloredSelection');
+                circle_2.classList.remove('coloredSelection');
+                circle_4.classList.remove('coloredSelection');
+                circle_5.classList.remove('coloredSelection');
+                break;
+            case 4:
+                circle_1.classList.remove('coloredSelection');
+                circle_2.classList.remove('coloredSelection');
+                circle_3.classList.remove('coloredSelection');
+                circle_5.classList.remove('coloredSelection');    
+                break;
+            case 5:
+                circle_1.classList.remove('coloredSelection');
+                circle_2.classList.remove('coloredSelection');
+                circle_3.classList.remove('coloredSelection');
+                circle_4.classList.remove('coloredSelection');  
+                break;  
         }
 }
 
@@ -59,5 +83,20 @@ circle_1.addEventListener('click',function(){
 
 circle_2.addEventListener('click',function(){
     ratePicker(2);
+    
+});
+
+circle_3.addEventListener('click',function(){
+    ratePicker(3);
+    
+});
+
+circle_4.addEventListener('click',function(){
+    ratePicker(4);
+    
+});
+
+circle_5.addEventListener('click',function(){
+    ratePicker(5);
     
 });
