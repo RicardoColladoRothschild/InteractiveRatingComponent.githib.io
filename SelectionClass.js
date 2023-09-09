68c2a9427f3ecfc9a2e3ebbe5cb59ab89269916b
ricardocollado <ricardocolladorothschild@gmail.com>
2023-09-09 02:17:16 -0400

    return to the original code, without class declarations

--- SelectionClass.js
+++ SelectionClass.js
@@ -1,65 +1,9 @@
-class SelectionClass {
+
     
    
 
-    colorSelection(selection){
-
-        switch(selection){
-            case 1:
-                circle_1.classList.add('coloredSelection');
-                break;
-            case 2:    
-                circle_2.classList.add('coloredSelection');
-                break;
-            case 3:
-                circle_3.classList.add('coloredSelection');
-                break;
-            case 4: 
-                circle_4.classList.add('coloredSelection');
-                break;        
-        
-            case 5:
-                circle_5.classList.add('coloredSelection');
-                break;
-            }
-    }
+    
 
 
 
-     deSelect(currentSelection){
-
-        switch(currentSelection){
-
-            case 1:
-                circle_2.classList.remove('coloredSelection');
-                circle_3.classList.remove('coloredSelection');
-                circle_4.classList.remove('coloredSelection');
-                circle_5.classList.remove('coloredSelection');
-                break;
-            case 2:
-                circle_1.classList.remove('coloredSelection');
-                circle_3.classList.remove('coloredSelection');
-                circle_4.classList.remove('coloredSelection');
-                circle_5.classList.remove('coloredSelection');
-                break;
-            case 3:
-                circle_1.classList.remove('coloredSelection');
-                circle_2.classList.remove('coloredSelection');
-                circle_4.classList.remove('coloredSelection');
-                circle_5.classList.remove('coloredSelection');
-                break;
-            case 4:
-                circle_1.classList.remove('coloredSelection');
-                circle_2.classList.remove('coloredSelection');
-                circle_3.classList.remove('coloredSelection');
-                circle_5.classList.remove('coloredSelection');    
-                break;
-            case 5:
-                circle_1.classList.remove('coloredSelection');
-                circle_2.classList.remove('coloredSelection');
-                circle_3.classList.remove('coloredSelection');
-                circle_4.classList.remove('coloredSelection');  
-                break;  
-        }
-}
-}
+     
