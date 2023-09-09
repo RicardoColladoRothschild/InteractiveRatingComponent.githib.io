6942726b5632c0aa9be4e1170c40ebaa9e07e77c
ricardocollado <ricardocolladorothschild@gmail.com>
2023-09-09 01:01:31 -0400

    change the setAttribute() where we change the clase to a different when a dv seleted. Now we add a classList.add() so the class will be adittional to the class, to later remove it.

--- main.js
+++ main.js
@@ -7,24 +7,57 @@ const circle_5 = document.querySelector('.circle5');
 
 
 
-function ratePicker(rating){
-    rate = rating;
-}
 
 
+//functions
 function colorSelection(selection){
 
     switch(selection){
         case 1:
-            circle_1.setAttribute('class','coloredSelection');
+            circle_1.classList.add('coloredSelection');
             break;
         case 2:    
-    }
+            circle_2.classList.add('coloredSelection');
+            break;
+        case 3:
+            circle_3.classList.add('coloredSelection');
+            break;
+        case 4: 
+            circle_4.classList.add('coloredSelection');
+            break;        
+    
+        case 5:
+            circle_5.classList.add('coloredSelection');
+            break;
+        }
 }
 
+function deSelect(currentSelection){
 
+        switch(currentSelection){
 
+            case 1:
+                circle_2.classList.remove('coloredSelection');
+                circle_3.classList.remove('coloredSelection');
+                circle_4.classList.remove('coloredSelection');
+                circle_5.classList.remove('coloredSelection');
+                break;
+        }
+}
+
+function ratePicker(rating){
+    rate = rating;
+    deSelect(rate);
+    colorSelection(rate);
+}
+
+//events
 circle_1.addEventListener('click',function(){
     ratePicker(1);
-    colorSelection(1);
+    
+});
+
+circle_2.addEventListener('click',function(){
+    ratePicker(2);
+    
 });
